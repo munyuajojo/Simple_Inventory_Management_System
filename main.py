2c3e2913f85fea08563ca0ffb75e19e47af08b3a
munyuajojo <jnmunyua@gmail.com>
2021-02-25 21:48:23 +0300

    UX, Installed SQLAlchemy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from configs.configurations import Development, Testing, Production
 
 app = Flask(__name__)
+app.config.from_object(Development)
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def index():
@@ -12,5 +18,6 @@ def admin():
 
 
 if __name__ =="__main__":
-
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug = True)
