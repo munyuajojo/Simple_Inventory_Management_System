1ca067bb3682705be5729e8a54089a9e93ed23a2
munyuajojo <jnmunyua@gmail.com>
2021-03-03 00:14:54 +0300

    models

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+#Configs
 from configs.configurations import Development, Testing, Production
 
 app = Flask(__name__)
 app.config.from_object(Development)
 db = SQLAlchemy(app)
 
+
+#Models
+from models.inventory import Inventory
+from models.stock import Stock
+from models.sales import Sales
+
+#Services
+from services.inventory import InventoryService
+
+#@app.before_first_request
+#def create():
+    #db.create_all()
+    
 
 
 @app.route('/')
@@ -14,7 +29,7 @@ def index():
 
 @app.route('/admin')
 def admin():
-    return render_template("/admin/admin.html")
+    return InventoryService.inventories()
 
 
 if __name__ =="__main__":
